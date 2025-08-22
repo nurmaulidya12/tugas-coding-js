f188737a0b289e79a60436834c09de5c1ad0a4df
nurmaulidya12 <syahrilmaulmana@gmail.com>
2025-08-22 11:20:44 +0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,3 @@
+function username(){
+            alert("Nama Lengkap: Nur Maulidiya \nHobi: Menyanyi \nUmur: 18 \nNama Sekolah: Smk Media Karya \nTTL: Jakarta 31 Maret 2007 ");
+        }
---
+++ div id.js
@@ -0,0 +1,5 @@
+//membuat objek elemen
+      var nama = document . getElementById("Tujuan");
+      
+      // menampilkan output ke elemen hasil
+      nama.innerHTML = "<p>keinginan :  Beli motor custom Bobber & Punya kerjaan yang bagus <br>Hobi : Menyanyi and Menari <br>Tujuan Hidup : Jadi orang sukses </p>";
